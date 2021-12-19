5ea10b8c390c891bf87674dc1b1253f8636a6081
developervws004 <developervws#4>
2021-12-19 20:12:22 +0530

    Initial commit

---
+++ app/src/main/java/com/example/flappybird/GameActivity.java
@@ -0,0 +1,14 @@
+package com.example.flappybird;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/example/flappybird/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.flappybird;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void startGame(View view)
+    {
+        Log.i("Button Click","Clicked");
+    }
+}
